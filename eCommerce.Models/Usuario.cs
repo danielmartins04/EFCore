d490d64738c62f727f34130033e85d6fb00c3455
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2023-04-05 21:37:22 -0300

    definindo entidades

---
+++ eCommerce.Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿namespace eCommerce.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Sexo { get; set; }
+        public string? Rg { get; set; }
+        public string? Cpf { get; set; }
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }
+        public ICollection<EnderecoEntrega>? EnderecosEntregas { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
+    }
+}
